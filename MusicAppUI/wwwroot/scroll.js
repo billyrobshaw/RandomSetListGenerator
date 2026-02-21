5ce41ea8f9e0500d2046d75e1ca58c1ecc727629
Billy <billyrobshaw2k3@gmail.com>
2026-02-21 18:27:16 +0000

    UI and auto scroll changes and added

---
+++ MusicAppUI/wwwroot/scroll.js
@@ -0,0 +1,17 @@
+﻿window.resetLyricsScroll = function (elementId) {
+    const box = document.getElementById(elementId);
+    if (!box) return;
+
+    box.scrollTop = 0;
+};
+
+
+window.scrollLyricsSmooth = function (elementId, amount) {
+    const box = document.getElementById(elementId);
+    if (!box) return;
+
+    box.scrollTop += amount;
+};
+
+
+
